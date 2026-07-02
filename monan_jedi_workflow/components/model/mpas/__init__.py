b3b5e516a0f9de997a24fe423cda91e16d623d34
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-07-02 07:03:50 -0300

    feat: export MPAS V2 contracts

--- monan_jedi_workflow/components/model/mpas/__init__.py
+++ monan_jedi_workflow/components/model/mpas/__init__.py
@@ -1,1 +1,21 @@
-"""MPAS product location utilities."""
+"""MPAS components used by MONAN-JEDI workflows."""
+
+from .forecast import MpasForecastStage
+from .output_validation import MpasOutputContract, validate_output_contract
+from .products import MpasForecastProduct, MpasForecastProductLayout, MpasProductLayoutError, normalize_mpas_time
+from .staging import LinkSpec, MpasStagingError, TemplateSpec, render_template, stage_link
+
+__all__ = [
+    "LinkSpec",
+    "MpasForecastProduct",
+    "MpasForecastProductLayout",
+    "MpasForecastStage",
+    "MpasOutputContract",
+    "MpasProductLayoutError",
+    "MpasStagingError",
+    "TemplateSpec",
+    "normalize_mpas_time",
+    "render_template",
+    "stage_link",
+    "validate_output_contract",
+]
