8968001ed6714faf62690be99267d8a6b8d227ce
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-07-02 00:35:06 -0300

    feat: export NMC pairs stage API

--- monan_jedi_workflow/components/bmatrix/nmc_pairs/__init__.py
+++ monan_jedi_workflow/components/bmatrix/nmc_pairs/__init__.py
@@ -1,8 +1,10 @@
 """NMC forecast-pair contracts for background-error covariance workflows."""
 
+from .config import NmcPairsConfigurationError, NmcPairsSettings
 from .manifest import BflowManifest, BflowManifestEntry, read_bflow_manifest, write_bflow_manifest
 from .model import NmcForecast, NmcPair, NmcPairError, normalize_time, plan_pairs
-from .validation import validate_pairs
+from .stage import NmcPairsStage
+from .validation import validate_bflow_manifest, validate_pairs
 
 __all__ = [
     "BflowManifest",
@@ -10,9 +12,13 @@ __all__ = [
     "NmcForecast",
     "NmcPair",
     "NmcPairError",
+    "NmcPairsConfigurationError",
+    "NmcPairsSettings",
+    "NmcPairsStage",
     "normalize_time",
     "plan_pairs",
     "read_bflow_manifest",
+    "validate_bflow_manifest",
     "validate_pairs",
     "write_bflow_manifest",
 ]
